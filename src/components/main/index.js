2614aa26dc1634a21df80b4e72a1064c6d8e51e8
Tyrice Clark <tyricec@civicresource.com>
2017-11-18 18:19:10 -0800

    move styles to style object

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -12,13 +12,20 @@ import {
   getCurrentRouteId,
 } from '../../reducers/index';
 
-const container = {
-  padding: '16px 8px',
-};
-
-const row = {
-  boxShadow: '0px 0px 2px rgb(0,0,0)',
-  marginBottom: '16px',
+const styles = {
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    padding: '16px 8px',
+  },
+  row: {
+    boxShadow: '0px 0px 2px rgb(0,0,0)',
+    marginBottom: '16px',
+    marginRight: '8px',
+    maxWidth: '480px',
+    width: '100%',
+  },
 };
 
 export class Main extends React.Component {
@@ -47,13 +54,13 @@ export class Main extends React.Component {
     }
 
     const routeDisplays = this.props.routes.map(route => (
-      <div key={route.route_id} style={row}>
+      <div key={route.route_id} style={styles.row}>
         <RouteDisplay onPress={() => this.onRouteClick(route.route_id)} route={route} />
       </div>
     ));
 
     return (
-      <div id="main" style={container}>
+      <div id="main" style={styles.container}>
         {routeDisplays}
       </div>
     );
